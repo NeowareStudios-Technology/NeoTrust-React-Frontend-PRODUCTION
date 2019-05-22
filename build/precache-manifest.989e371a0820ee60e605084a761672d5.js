a0f44b110507c8ba2fae4ef23356d5c4a6617c76
David Ramirez <ramirdav@oregonstate.edu>
2019-05-22 15:46:38 -0400

    working build created!!!

---
+++ build/precache-manifest.989e371a0820ee60e605084a761672d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "05017aa8734c03e9951fb42575bc8c8c",
+    "url": "/static/media/logo.05017aa8.png"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "29740e29c17d6b8788c0",
+    "url": "/static/js/main.29740e29.chunk.js"
+  },
+  {
+    "revision": "690826e7eee71715c95f",
+    "url": "/static/js/1.690826e7.chunk.js"
+  },
+  {
+    "revision": "29740e29c17d6b8788c0",
+    "url": "/static/css/main.da2ef88c.chunk.css"
+  },
+  {
+    "revision": "690826e7eee71715c95f",
+    "url": "/static/css/1.19184c02.chunk.css"
+  },
+  {
+    "revision": "579e0eb395db59299ffaab2dc9223a39",
+    "url": "/index.html"
+  }
+];
